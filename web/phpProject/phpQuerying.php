732092a6a9ed8f1e953de368dd3ab71134c8a0d6
carlos <carlosbarreraojeda@gmail.com>
2018-02-09 17:09:56 -0600

    atempting to display a table

--- web/phpProject/phpQuerying.php
+++ web/phpProject/phpQuerying.php
@@ -63,9 +63,11 @@ else{
     $sqlSubjectId = "SELECT subject_id FROM public.user_subjects WHERE user_id =".$_SESSION["userId"];
     foreach ($db->query($sqlSubjectId) as $row) {
        $sqlSubject = "SELECT subject_name FROM public.subject WHERE id =".$row["subject_id"];
-       foreach ($db->query($sqlSubject) as $column){
+       /*foreach ($db->query($sqlSubject) as $column){
             echo "<input type='button' value=".$column["subject_name"]."><br/>";
-        }
+        }*/
+       $column = $db->query($sqlSubject);
+        echo "<input type='button' value=".$column[0]["subject_name"]."><br/>";
     }
     echo "</from>";
 }
