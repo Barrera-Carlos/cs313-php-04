9eb210b05b19e15aeb2862408d40b620345d054a
carlos <carlosbarreraojeda@gmail.com>
2018-02-13 21:02:38 -0600

    adding bootStrap to the php page

--- web/phpProject/phpQuerying.php
+++ web/phpProject/phpQuerying.php
@@ -30,6 +30,9 @@ catch (PODException $ex){
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title>Scripture List</title>
 </head>
 
