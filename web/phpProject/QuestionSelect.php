19439b42cd6130e1db8020d0d8d859a680548d83
carlos <carlosbarreraojeda@gmail.com>
2018-02-19 19:15:27 -0600

    testing query answer for deleting bundles

--- web/phpProject/QuestionSelect.php
+++ web/phpProject/QuestionSelect.php
@@ -74,7 +74,7 @@ catch (PODException $ex){
             $bundleID = "SELECT id FROM public.bundle_name WHERE bundle_name =".$_POST['bundle'];
             echo $bundleID.'</br>';
             $bundleIdAnswer = $db->query($bundleID);
-            echo $bundleID['id'][0];
+            echo $bundleID['id'][1];
         }
     }
 
