02f5de048b9476a4107057fd78eaa24502ae9ff9
carlos <carlosbarreraojeda@gmail.com>
2018-02-22 12:48:36 -0600

    working on the display function of the quiz display

--- web/phpProject/quiz.php
+++ web/phpProject/quiz.php
@@ -5,4 +5,53 @@
  * Date: 2/11/2018
  * Time: 7:35 PM
  */
-echo "under development";
+session_start();
+
+$dbURL = getenv('DATABASE_URL');
+$dbopts = parse_url($dbURL);
+
+
+try{
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PODException $ex){
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Title</title>
+    <style>
+    </style>
+    <script>
+    </script>
+</head>
+<body>
+<?php
+    echo "<div class=\"container\" id='inputContainer'>";
+    echo "<form action=\"QuestionSelect.php\" method='post'>";
+    echo "<div class=\"row\">";
+    echo "<div class=\"col-sm-4\" id='inputRow'><input type='text' name='inputAnswer'></div>";
+    echo "<div class=\"col-sm-4\" id='inputRow'><input type='text' name='inputQuestion'></div>";
+    echo "<div class=\"col-sm-4\" id='inputRow'><input type='button' name='inputQuestion' onclick=''></div>";
+    echo "<input type='submit' style='display: none' id='submit'>";
+    echo "</div>";
+    echo "</form>";
+    echo "</div>";
+
+?>
+</body>
+</html>
