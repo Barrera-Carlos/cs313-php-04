9dff2e82616249672930550cfe2a656cc54fa091
carlos <carlosbarreraojeda@gmail.com>
2018-02-23 16:39:11 -0600

    working on displaying newly added questions

--- web/phpProject/quiz.php
+++ web/phpProject/quiz.php
@@ -106,6 +106,7 @@ catch (PODException $ex){
                 for ($x=0; $x < $c; $x++){
                     $questionIdString = "SELECT id FROM questions WHERE question='".$_POST['question'][$x]."'";
                     foreach ($db->query($questionIdString) as $item){
+                        array_push($questionIdArray,$item['id']);
                         $delete1 = "DELETE FROM bundle_questions WHERE question_id=".$item['id'];
                         $delete2 = "DELETE FROM question_answers WHERE question_id=".$item['id'];
                         $delete3 = "DELETE FROM questions WHERE id=".$item['id'];
@@ -119,12 +120,19 @@ catch (PODException $ex){
                 }
             }
             if(isset($_POST['answer'])){
-                foreach ($_POST['answer'] as $value){
-                    $delete1 = "SELECT id FROM answer WHERE answer ='".$value['answer']."'";
-                    echo $delete1."</br>";
-                    foreach ($db->query($delete1) as $item){
-                        array_push($questionIdArray,$item['id']);
-                        echo $item['id']."</br>";
+                $c= count($_POST['answer']);
+                for ($x=0; $x < $c; $x++){
+                    $answerIdString = "SELECT id FROM answer WHERE answer='".$_POST['answer'][$x]."'";
+                    foreach ($db->query($answerIdString) as $item){
+                        array_push($answerIdArray,$item['id']);
+                        $delete1 = "DELETE FROM bundle_questions WHERE question_id=".$item['id'];
+                        $delete2 = "DELETE FROM question_answers WHERE question_id=".$item['id'];
+                        $delete3 = "DELETE FROM questions WHERE id=".$item['id'];
+                        $delete4 = "DELETE FROM answer WHERE id=".$item['id'];
+                        $db->query($delete1);
+                        $db->query($delete2);
+                        $db->query($delete3);
+                        $db->query($delete4);
                     }
                 }
             }
