014e4baa58b94ccf8cb20e8ac97ae709d9e82ae6
carlos <carlosbarreraojeda@gmail.com>
2018-02-24 13:48:11 -0600

    testing sign up logic

--- web/phpProject/inputUserInformation.php
+++ web/phpProject/inputUserInformation.php
@@ -5,3 +5,7 @@
  * Date: 2/24/2018
  * Time: 11:37 AM
  */
+
+    $psw = password_hash($_POST['psw'], PASSWORD_DEFAULT);
+    $subString = "INSERT INTO public.user (username,password,display_name) VALUES('".$_POST['username']."','".$psw."','".$_POST['dName']."')";
+    echo $subString;
