585d44af239c8ebb541e87c13fdc374c4cdfeb88
carlos <carlosbarreraojeda@gmail.com>
2018-02-24 13:59:27 -0600

    testing sign up logic

--- web/phpProject/inputUserInformation.php
+++ web/phpProject/inputUserInformation.php
@@ -6,6 +6,24 @@
  * Time: 11:37 AM
  */
 
+    $dbURL = getenv('DATABASE_URL');
+    $dbopts = parse_url($dbURL);
+
+
+    try{
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PODException $ex){
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+
     $psw = password_hash($_POST['psw'], PASSWORD_DEFAULT);
     $subString = "INSERT INTO public.user (username,password,display_name) VALUES('".$_POST['username']."','".$psw."','".$_POST['dName']."')";
-    echo $subString;
+    $db->query($subString);
