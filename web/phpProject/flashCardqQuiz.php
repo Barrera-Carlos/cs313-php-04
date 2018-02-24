16a6707d73b44eca89f426419e6dabbfd36b6594
carlos <carlosbarreraojeda@gmail.com>
2018-02-23 23:26:59 -0600

    testing card display

--- web/phpProject/flashCardqQuiz.php
+++ web/phpProject/flashCardqQuiz.php
@@ -54,7 +54,7 @@
         $question = "SELECT question FROM question WHERE id =".$questionId['question_id'];
 
         foreach ($db->query($question) as $item){
-            echo $item['question']."</br>";
+            //echo $item['question']."</br>";
             array_push($questionArray, $item['question']);
 
         }
