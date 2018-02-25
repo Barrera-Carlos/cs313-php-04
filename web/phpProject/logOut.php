9a5ac21c6f81767cab8b12247f666e3001c0a8c5
carlos <carlosbarreraojeda@gmail.com>
2018-02-24 23:17:52 -0600

    im out boy

---
+++ web/phpProject/logOut.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 2/24/2018
+ * Time: 11:16 PM
+ */
+
+session_start();
+
+
+$_SESSION = array();
+
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+
+session_destroy();
